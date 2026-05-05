ef48aba4f4a271b44df3b39cd489f9143278196b
LUIZ DA ANUNCIACAO CONCEICAO <luiz.aconceicao2@senacsp.edu.br>
2026-05-04 21:55:34 -0300

    Adiciona rota GET para listar todas as pizzas

--- config/Database.php
+++ config/Database.php
@@ -1,37 +1,47 @@
 <?php
-class Database {
+ 
+// config/Database.php
+class Database # definição da classe Database
+{
+    // variáveis privadas para armazenar as informações de conexão com o banco de dados
     private $host = 'localhost';
-    private $db_name = 'jucapizzasdb';
+    private $db_name = 'test';
     private $username = 'root';
     private $password = 'usbw';
     private $port = '3311';
  
+    // variável pública para armazenar a conexão com o banco de dados
     public $conn;
  
- 
-    public function getConnection(){
- 
+    // método para obter a conexão com o banco de dados
+    public function getConnection()
+    {
+        // inicializa a variável de conexão como nula
         $this->conn = null;
  
+        //tenta executar um codigo que pode gerar um erro
         try {
-            //tenta executar um código potencialmente perigoso
-            // DSN (Data Source Name) - String de conexão
-            $dsn = 'mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name . ';charset=utf8';
  
-            // Instancia o objeto PDO
+            # string de conexão com o banco de dados usando o formato DSN (Data Source Name)
+            $dsn = 'mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db_name . ';charset=utf8'; #adiciona o charset utf8 à string de conexão
+ 
+            # Instancia a classe PDO para estabelecer a conexão com o banco de dados usando as informações fornecidas
             $this->conn = new PDO($dsn, $this->username, $this->password);
  
-            // Define o modo de erro do PDO para exceção
-            // Isso faz com que o PDO lance exceções em caso de erros,
-            // facilitando o tratamento
+                # Configura o modo de erro do PDO para lançar exceções em caso de erros de conexão ou consulta
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       
-        } catch (PDOException $e) {
-             // Em caso de erro na conexão, exibe a mensagem de erro
-            echo 'Erro de Conexão: ' . $e->getMessage();
-        } catch (Throwable $e) {
-            echo 'Erro genérico: '. $e->getMessage();
+ 
+        } catch (PDOException $e) #captura o erro e armazena na variavel $e | PDOException é um tipo de erro específico para conexões com banco de dados
+ 
+        {
+            echo "Erro de Conexão: " . $e->getMessage(); #exibe a mensagem de erro
+        } catch (Exception $e) #captura qualquer outro tipo de erro
+        {
+            echo "Erro: " . $e->getMessage(); #exibe a mensagem de erro
         }
-        return $this->conn;
+ 
+        return $this->conn; #retorna a conexão com o banco de dados
     }
 }
+ 
+ 
